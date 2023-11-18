28ab877844ad783fccc80b192611c4f9d7f61df9
robisoncrisbrito <robison.brito@gmail.com>
2023-11-18 09:54:02 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/usandofirebase_pos2023/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.utfpr.usandofirebase_pos2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
